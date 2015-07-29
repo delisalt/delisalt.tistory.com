a6d86319e2fec392c536d21f64c3509cc2322949
quizkidj <salt.jeong@gmail.com>
2015-07-30 04:16:49 +0900

    Version 1.0.0

--- images/delisalt_bootstrap.js
+++ images/delisalt_bootstrap.js
@@ -1,5 +1,6 @@
 $( document ).ready(function(){
-
+	var host = "http://blog.delisalt.me"; // TO BE CHANGED
+	
 	//navigation bar
 	$('#category_view').prepend('<li><a href="/category">' + $('#category-list').children().children().children().html() + '</a></li>');
 	$('#category_view').append($('#category-list').children().children().children().next().html());
@@ -12,32 +13,32 @@ $( document ).ready(function(){
 		$('#ttoolbar_view').prepend('<li><a title="add blog to Link" class="tt_add_link" href="/toolbar/popup/link/"><span class="glyphicon glyphicon-link"></span></a></li>');
 
 		temp = $('.tt_menubar_box_btn').html();
-		if( temp.search('http://delisalt.tistory.com/admin/') != -1){
-			$('#ttoolbar_view').prepend('<li><a title="admin acount" href="http://delisalt.tistory.com/admin/"><span class="glyphicon glyphicon-user"></span></a></li>');
-			$('#ttoolbar_view').prepend('<li><a title="new post" href="http://delisalt.tistory.com/admin/entry/post/"><span class="glyphicon glyphicon-plus"></span></a></li>');
+		if( temp.search('/admin/') != -1){
+			$('#ttoolbar_view').prepend('<li><a title="admin acount" href="' + host + '/admin/"><span class="glyphicon glyphicon-user"></span></a></li>');
+			$('#ttoolbar_view').prepend('<li><a title="new post" href="' + host + '/admin/entry/post/"><span class="glyphicon glyphicon-plus"></span></a></li>');
 		}
 	}else{ //log-out
 		$('#ttoolbar_view').append('<li><a href="https://www.tistory.com/login">Log in</a></li>');
 	}
 
 	//fill title
-	if(url.indexOf('delisalt.tistory.com/search') != -1 
-		|| url.indexOf('delisalt.tistory.com/tag/') != -1 
-		|| url.indexOf('delisalt.tistory.com/archive/') != -1){
+	if(url.indexOf('/search') != -1 
+		|| url.indexOf('/tag/') != -1 
+		|| url.indexOf('/archive/') != -1){
 		//search list
-		$('#article_category').html('&nbsp&nbsp<span class="glyphicon glyphicon-search ft-size-15"></span>');
+		$('#article_category').html('&nbsp;&nbsp;<span class="glyphicon glyphicon-search ft-size-15"></span>');
 		$('#article_category').prepend($('#search_word').text());
 		$('#article_title').text('글 검색 결과');
 		$('#article_date').append('총 ' + $('#search_count').text() + '개');
-	}else if(url.indexOf('delisalt.tistory.com/tag') != -1){
+	}else if(url.indexOf('/tag') != -1){
 		//tag list
 		$('#article_category').html('');
 		$('#article_title').text('꼬리표tag 목록');
-	}else if(url.indexOf('delisalt.tistory.com/guestbook') != -1 || url.indexOf('delisalt.tistory.com/guestbook/') != -1){
+	}else if(url.indexOf('/guestbook') != -1){
 		//guestbook
 		$('#article_category').html('');
 		$('#article_title').text('오고간 흔적들');
-	}else if(url.indexOf('delisalt.tistory.com/category') != -1 || url.indexOf('delisalt.tistory.com/category/') != -1){
+	}else if(url.indexOf('/category') != -1 ){
 		//category
 		$('#article_category').prepend($('#search_word').text());
 		$('#article_title').text('분류별 검색 결과');
